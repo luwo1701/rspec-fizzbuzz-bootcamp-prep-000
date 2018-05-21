632af420b85cdd660db5898d76dbee09848d0e82
Luke Worley <luke.worley@colorado.edu>
2018-05-21 17:28:52 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,21 @@
+def fizzbuzz(n)
+  puts "hello"
+  string=""
+  if n%3 == 0
+    puts "Fizz"
+    string = string + "Fizz"
+  elsif n%5 == 0
+    puts "Buzz"
+    string= string+"Buzz"
+    #return "Buzz"
+  else
+    string = string + "nil"
+    return nil
+  end
+  puts string
+  return string
+end
+
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
